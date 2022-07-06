540c469b37c2c864e831d8cc8a127428d2724cc1
JSD <suryadharma2@gmail.com>
2022-07-06 14:48:02 +0700

    add utility to make easier to encrypt

---
+++ utility/bcrypt.js
@@ -0,0 +1,13 @@
+const bcrypt = require('bcrypt');
+
+const hashPassword = (userPassword) => {
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(userPassword, salt);
+  return hash;
+};
+
+const comparePassword = (userPassword, dbPassword) => {
+  return bcrypt.compareSync(userPassword, dbPassword);
+};
+
+module.exports = { hashPassword, comparePassword };
