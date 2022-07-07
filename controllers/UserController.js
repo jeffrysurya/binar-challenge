174f402fa60813897ccdf85f0529e34f92dc6d33
JSD <suryadharma2@gmail.com>
2022-07-07 17:21:56 +0700

    add login mechanisme

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -33,67 +33,78 @@ class UserAuthController {
                 message: `Invalid Username or Password`,
             })
           }
-        const isMatch = await comparePassword(password);
+        const isMatch = await comparePassword(password, user.password);
             if (isMatch) {
-
+                const payload = {
+                    id: user.id,
+                    username: user.username,
+                    role: user.role,
+                };
+                const access_token = generateToken(payload);
+                return res.status(200).json({
+                    access_token: access_token,
+                });
+            } else {
+                return res.status(401).json({
+                    message: `Invalid Username or Password`,
+                });
             }
 
-        } catch {
-
+        } catch(err) {
+            return res.status(401).json(err);
+        }
         }
     }
-    }
 
-exports.validateUser = (req, res) => {
-    const { email, password } = req.body;
-    const validEmail = UserAuth.findOne({
-        where: {email}
-    }).then((data) => {
-        const validPassword = password === data.password;
-        console.log(data.password)
-        if (!validPassword) {
-            res.json({
-                message: "password is not valid"
-            })
-        } else {
-            res.json({
-                message: "you have access"
-            })
-        }
-    })
-}
+    module.exports = {UserAuthController }
+// exports.validateUser = (req, res) => {
+//     const { email, password } = req.body;
+//     const validEmail = UserAuth.findOne({
+//         where: {email}
+//     }).then((data) => {
+//         const validPassword = password === data.password;
+//         console.log(data.password)
+//         if (!validPassword) {
+//             res.json({
+//                 message: "password is not valid"
+//             })
+//         } else {
+//             res.json({
+//                 message: "you have access"
+//             })
+//         }
+//     })
+// }
 
-exports.getAllUser = async (req, res) => {
-    const user = await UserAuth.findAll({raw: true}).then(user => {
+// exports.getAllUser = async (req, res) => {
+//     const user = await UserAuth.findAll({raw: true}).then(user => {
         
-        const data = JSON.stringify(user,null,2)
-        console.log(data.user_id)
-        return res.render('dashboard', {data})})
-        .then(data => res.json(data))
-//         .catch(err => res.status(400).json(err));
-}
+//         const data = JSON.stringify(user,null,2)
+//         console.log(data.user_id)
+//         return res.render('dashboard', {data})})
+//         .then(data => res.json(data))
+// //         .catch(err => res.status(400).json(err));
+// }
 
-exports.updatePassword = (req, res) => {
-    const {email,password} = req.body;
-    UserAuth.update({
-        password
-    }, {
-        where: { email }
-    }).then(() => 
-    res.json({
-        message: "Password Successfully updated"
-    }))
-}
+// exports.updatePassword = (req, res) => {
+//     const {email,password} = req.body;
+//     UserAuth.update({
+//         password
+//     }, {
+//         where: { email }
+//     }).then(() => 
+//     res.json({
+//         message: "Password Successfully updated"
+//     }))
+// }
 
-exports.deleteUser = (req, res) => {
-    const {email,password} = req.body;
+// exports.deleteUser = (req, res) => {
+//     const {email,password} = req.body;
     
-    UserAuth.destroy({
-        where: { email, password }
-    }).then(() => 
-    res.json({
-        message: "user successfully deleted"
-    }))
-}
-
-module.exports = {UserAuthController }
+//     UserAuth.destroy({
+//         where: { email, password }
+//     }).then(() => 
+//     res.json({
+//         message: "user successfully deleted"
+//     }))
+// }
